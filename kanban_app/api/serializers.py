af039117a4de41d1c7e599bb7bd2456ac6c2671a
Ricardo <ricardo-pinto@web.de>
2025-07-26 12:52:41 +0200

    Implement Post/api/boards/endpoint

--- kanban_app/api/serializers.py
+++ kanban_app/api/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from kanban_app.models import Board
 
 class BoardSerializer(serializers.ModelSerializer):
+    owner_id = serializers.IntegerField(source='owner.id')
     member_count = serializers.SerializerMethodField()
     ticket_count = serializers.SerializerMethodField()
     tasks_to_do_count = serializers.SerializerMethodField()
@@ -9,16 +10,24 @@ class BoardSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Board
-        fields = ['id', 'title', 'owner', 'member_count', 'ticket_count', 'tasks_to_do_count', 'tasks_high_prio_count']
+        fields = [
+            'id',
+            'title',
+            'owner_id',
+            'member_count',
+            'ticket_count',
+            'tasks_to_do_count',
+            'tasks_high_prio_count'
+        ]
 
     def get_member_count(self, obj):
         return obj.members.count()
 
     def get_ticket_count(self, obj):
-        return obj.task_set.count()
+        return 0  # Task-Modell noch nicht vorhanden
 
     def get_tasks_to_do_count(self, obj):
-        return obj.task_set.filter(status='to-do').count()
+        return 0
 
     def get_tasks_high_prio_count(self, obj):
-        return obj.task_set.filter(priority='high').count()
+        return 0
